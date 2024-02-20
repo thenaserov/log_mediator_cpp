cdb8de1226bbdbebf16912cc722388ddc40386ef
thenaserov <naser.rezayi.nr@gmail.com>
2024-02-20 17:01:56 +0330

    [ADD] fileoperator

---
+++ fileoperator.cpp
@@ -0,0 +1,3 @@
+#include "fileoperator.h"
+
+FileOperator::FileOperator() {}
---
+++ fileoperator.h
@@ -0,0 +1,10 @@
+#ifndef FILEOPERATOR_H
+#define FILEOPERATOR_H
+
+class FileOperator
+{
+public:
+    FileOperator();
+};
+
+#endif // FILEOPERATOR_H
