2ffe11d45cfe9d4e0b5b7b5c06307a7021829297
Naser Rezayi <54706692+thenaserov@users.noreply.github.com>
2024-02-21 15:22:14 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
-/*
-Assuming that you have added C++ REST SDK to your project successfully:
-This is how you can POST a JSON to your http://localhost:9200 server
-Easy AF
-  */
+#include "iostream"
 
-
-#include <iostream>
+#include "Controller.h"
 
 
 int main() {
-    return 0;
+	Controller cntr;
+	cntr.getLog();
+	return 0;
 }
