468f0503a13551509f17b1c8801fda2296072713
Naser Rezayi <54706692+thenaserov@users.noreply.github.com>
2024-02-24 16:30:18 +0330

    Update Controller.cpp

--- Controller.cpp
+++ Controller.cpp
@@ -16,12 +16,32 @@ void Controller::getLog()
 
 void Controller::checkConnection()
 {
+	_hb.ping("google.com");
 }
 
 void Controller::logToFile()
 {
+
 }
 
 void Controller::logToServer()
 {
+}
+
+void Controller::operate()
+{
+	// get log
+	while (1) {
+
+	}
+
+	// check elastic server
+	while (1) {
+		
+	}
+
+	// log the logs
+	while (1) {
+
+	}
 }
