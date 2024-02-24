b370907d53d23cabe1823add60a0ca6b945c47e4
Naser Rezayi <54706692+thenaserov@users.noreply.github.com>
2024-02-24 16:30:36 +0330

    Update Controller.h

--- Controller.h
+++ Controller.h
@@ -1,19 +1,38 @@
 #pragma once
 #include "FileOperator.h"
+#include "HeartBeat.h"
 #include <vector>
+#include <thread>
+#include <mutex>
 
 class Controller{
 
 public:
 	Controller();
 	std::vector<std::string> readLogsFromFile(std::string path);
-	void getLog(); // get from anywhere and give it to the LogGenerator
+
+	// _thread_log_check
+	void getLog();
+
+	// heartbeat
 	void checkConnection();
+
+	// _thread_do_log
+	void doLog();
+
 	void logToFile();
 	void logToServer();
 
+	// main
+	void operate();
+
 private:
-	FileOperator _file_operator{};
+	FileOperator _file_operator;
+	HeartBeat _hb;
+	std::mutex _mtx;
+	std::thread _thread_heartbeat;
+	std::thread _thread_log_check;
+	std::thread _thread_do_log;
 
 protected:
 };
