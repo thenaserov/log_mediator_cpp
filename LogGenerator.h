5cc6e79e5f6f45307179f2de86e1feb18c66a782
Naser Rezayi <54706692+thenaserov@users.noreply.github.com>
2024-02-24 16:32:13 +0330

    Update LogGenerator.h

--- LogGenerator.h
+++ LogGenerator.h
@@ -1,12 +1,22 @@
 #pragma once
+#include <string>
+#include <vector>
+
 
 class LogGenerator {
 
 public:
 	LogGenerator();
 	~LogGenerator();
+	void regenerateLog();
+	std::string gettLogAsString();
+	void addAnElementToCurrentLog(std::string element);
+	void clearLogsVector();
 
 private:
+	std::string _log = "";
+	std::string _full_log = "";
+	std::vector<std::string> _logs_vector;
 
 protected:
 
