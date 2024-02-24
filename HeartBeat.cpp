9fa9158a958fc1acd8888f7c33916cd6faa0c477
Naser Rezayi <54706692+thenaserov@users.noreply.github.com>
2024-02-24 16:31:28 +0330

    Update HeartBeat.cpp

--- HeartBeat.cpp
+++ HeartBeat.cpp
@@ -6,5 +6,14 @@ HeartBeat::HeartBeat()
 
 HeartBeat::~HeartBeat()
 {
-	delete this;
+}
+
+bool HeartBeat::ping(std::string address)
+{
+	std::string data = "ping " + address;
+	if (system(data.c_str()) == 0) {
+		return true;
+	} else {
+		return false;
+	}
 }
