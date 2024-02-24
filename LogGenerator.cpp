d346267ad361b7e561a3e62421c9b101264ce1ec
Naser Rezayi <54706692+thenaserov@users.noreply.github.com>
2024-02-24 16:31:55 +0330

    Update LogGenerator.cpp

--- LogGenerator.cpp
+++ LogGenerator.cpp
@@ -6,5 +6,28 @@ LogGenerator::LogGenerator()
 
 LogGenerator::~LogGenerator()
 {
-	delete this;
+}
+
+void LogGenerator::regenerateLog()
+{
+	for (std::vector<std::string>::iterator it = _logs_vector.begin(); it != _logs_vector.end(); it++) {
+		_full_log += *it + "|";
+	}
+	_full_log.pop_back(); // This removes the last "|"
+}
+
+std::string LogGenerator::gettLogAsString()
+{
+	regenerateLog();
+	return _full_log;
+}
+
+void LogGenerator::addAnElementToCurrentLog(std::string element)
+{
+	_logs_vector.emplace_back(element);
+}
+
+void LogGenerator::clearLogsVector()
+{
+	_logs_vector.clear();
 }
